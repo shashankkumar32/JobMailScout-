c9e831bb9b41feaefa3f7023b2339d7305498074
shashankkumar32 <shashankofficial3209@gmail.com>
2025-01-04 15:00:18 +0530

    production-fix2

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -14,7 +14,7 @@ export default async function DashboardLayout({ children }) {
 <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 text-sm text-center md:text-4xl lg:text-2xl shadow-lg">
       {/* <h1>Dashboard</h1> */}
       <TwoColumnLayout>
-
+{/* ldfkldkf */}
       {children}
 
       </TwoColumnLayout>
